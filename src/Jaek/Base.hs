c9487b70d4a7f02115f37b40fb0c82235433e23a
jwlato <jwlato@gmail.com>
2011-11-04 11:32:22 +0000

    add Jaek.Base.tpass (trace utility function)

--- src/Jaek/Base.hs
+++ src/Jaek/Base.hs
@@ -9,6 +9,7 @@ module Jaek.Base (
  ,liftIO
  ,ignore
  ,annMaybe
+ ,tpass
  ,module X
 )
 
@@ -20,6 +21,9 @@ import Control.Applicative as X
 import Control.Monad       as X
 import Control.Monad.Error
 import Control.Monad.Trans.Class as X
+
+import Debug.Trace
+import Text.Printf
 
 type SampleCount = Int
 type ChanNum = Int
@@ -39,3 +43,7 @@ ignore m = m >> return ()
 
 annMaybe :: (Monad m, Functor m) => String -> m (Maybe a) -> ErrorT String m a
 annMaybe s m = ErrorT $ fmap (maybe (Left s) Right) m
+
+-- | Adds a trace to `a` with the given label
+tpass :: Show a => String -> a -> a
+tpass lbl a = trace (printf (lbl ++ ": %s\n") (show a)) a
